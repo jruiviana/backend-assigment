f2f7093ce49e9ef99e2d89363c6c1e9add824d57
José Viana <Jose.Viana@swisscom.com>
2021-02-07 16:29:08 +0100

    Add API project files

---
+++ Movies/Movies.API/Controllers/MoviesController.cs
@@ -0,0 +1,49 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Movies.Feature.Administration.Commands;
+using Movies.Feature.Queries;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Movies.API.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class MoviesController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        private readonly ILogger<MoviesController> _logger;
+
+        public MoviesController(ILogger<MoviesController> logger, IMediator mediator)
+        {
+            _mediator = mediator;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAsync([FromQuery] GetMovieFromSourceQuery query, CancellationToken cancellationToken)
+        {
+            _logger.LogInformation($"Search for {query.Title}");
+            var sw = Stopwatch.StartNew();
+            var result = await _mediator.Send(query, cancellationToken);
+            sw.Stop();
+
+            // Create search entry in the database
+            await _mediator.Send(
+                new CreateSearchCommand(
+                    query.Title,
+                    result is null ? string.Empty : result.ImdbID,
+                    sw.ElapsedMilliseconds,
+                    HttpContext.Connection.RemoteIpAddress.ToString()), cancellationToken);
+
+            if (result is null)
+            {
+                return NotFound("Movie not found!");
+            }
+
+            return Ok(result);
+        }
+    }
+}
--- Movies/Movies.API/Startup.cs
+++ Movies/Movies.API/Startup.cs
@@ -8,6 +8,14 @@ using Movies.Core.Interfaces;
 using Movies.Feature;
 using Movies.Feature.Services;
 using MediatR;
+using Movies.API.Middleware;
+using Movies.Core.Models;
+using Microsoft.Extensions.Options;
+using Movies.Feature.Administration.Services;
+using AutoMapper;
+using Movies.API.Authentication;
+using Microsoft.AspNetCore.Localization;
+using System.Globalization;
 
 namespace Movies.API
 {
@@ -23,8 +31,19 @@ namespace Movies.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.Configure<SearchesDatabaseSettings>(
+                     Configuration.GetSection(nameof(SearchesDatabaseSettings)));
+            services.Configure<MovieSources>(
+                     Configuration.GetSection(nameof(MovieSources)));
+
             // DI
+            services.AddSingleton<ISearchesDatabaseSettings>(sp =>
+               sp.GetRequiredService<IOptions<SearchesDatabaseSettings>>().Value);
+            services.AddSingleton<IMovieSources>(sp =>
+               sp.GetRequiredService<IOptions<MovieSources>>().Value);
             services.AddTransient<ISourceMovieService, SourceMovieService>();
+            services.AddTransient<IHttpHandler, HttpClientService>();
+            services.AddSingleton<IMongoDBService, MongoDBService>();
 
             // MediatR
             services.AddMediatR(typeof(FeaturesModule).Assembly, typeof(CoreModule).Assembly);
@@ -32,6 +51,15 @@ namespace Movies.API
             services.AddControllers();
 
             services.AddSwaggerGen();
+
+            // Auto-mapping
+            services.AddAutoMapper(typeof(CoreModule));
+
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = ApiKeyAuthenticationOptions.DefaultScheme;
+                options.DefaultChallengeScheme = ApiKeyAuthenticationOptions.DefaultScheme;
+            }).AddApiKeySupport(options => { });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -47,6 +75,10 @@ namespace Movies.API
             app.UseRouting();
 
             app.UseAuthorization();
+            app.UseAuthentication();
+
+            // Middleware
+            app.UseMiddleware<ErrorHandlingMiddleware>();
 
             app.UseSwagger();
 
