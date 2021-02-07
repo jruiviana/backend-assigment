6b1d8d58767d21afdd2939c8771a08fecb10b8f3
José Viana <Jose.Viana@swisscom.com>
2021-02-07 16:24:07 +0100

    Add Middleware to handle the exceptions

---
+++ Movies/Movies.API/Middlewares/ErrorHandlingMiddleware.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+namespace Movies.API.Middleware
+{
+    public class ErrorHandlingMiddleware
+    {
+        private readonly ILogger<ErrorHandlingMiddleware> _logger;
+        private readonly RequestDelegate _next;
+
+        public ErrorHandlingMiddleware(RequestDelegate next, ILogger<ErrorHandlingMiddleware> logger)
+        {
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                await HandleExceptionAsync(context, ex);
+            }
+        }
+
+        private async Task HandleExceptionAsync(HttpContext context, Exception exception)
+        {
+            HttpStatusCode code;
+            var message = exception.Message;
+
+            switch (exception)
+            {
+                case ValidationException ex:
+                    code = HttpStatusCode.BadRequest;
+                    _logger.LogWarning(exception, $"Bad Request: {ex.Message} - {context.Request.Method} {context.Request.Path + context.Request.QueryString}.");
+                    break;
+                default:
+                    code = HttpStatusCode.InternalServerError;
+                    message = "Something went wrong during request handling";
+                    _logger.LogError(exception, $"Error: {exception.Message}.");
+                    break;
+            }
+
+            context.Response.ContentType = "text/plain";
+            context.Response.StatusCode = (int)code;
+            await context.Response.WriteAsync(message);
+        }
+    }
+}
---
+++ Movies/Movies.Tests/MiddlewareTests/ErrorHandlingMiddlewareTests.cs
@@ -0,0 +1,86 @@
+﻿using System.Threading.Tasks;
+using Xunit;
+using System;
+using System.ComponentModel.DataAnnotations;
+using FakeItEasy;
+using Movie.Tests;
+using Movies.API.Middleware;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Http;
+
+namespace Movies.Tests.MiddlewareTests
+{
+    public class ErrorHandlingMiddlewareTests : BaseTest
+    {
+        private readonly ILogger<ErrorHandlingMiddleware> _logger;
+        private readonly RequestDelegate _next;
+        private readonly DefaultHttpContext _httpContext;
+        private readonly ErrorHandlingMiddleware _errorHandlingMiddleware;
+
+        public ErrorHandlingMiddlewareTests()
+        {
+            _next = A.Fake<RequestDelegate>();
+            _httpContext = new DefaultHttpContext();
+            _logger = A.Fake<ILogger<ErrorHandlingMiddleware>>();
+            _errorHandlingMiddleware = new ErrorHandlingMiddleware(_next, _logger);
+        }
+
+        [Fact]
+        public async Task Invoke_must_call_requestDelegate_invoker()
+        {
+            //Arrange
+            A.CallTo(() => _next.Invoke(A<HttpContext>.Ignored)).Returns(Task.FromResult(1));
+
+            //Act
+            await _errorHandlingMiddleware.Invoke(_httpContext);
+
+            //Assert
+            A.CallTo(() => _next.Invoke(A<HttpContext>.Ignored)).MustHaveHappened();
+        }
+
+        [Fact]
+        public async Task Invoke_must_log_warning_and_set_HttpStatusCode_badRequest_on_ValidationException()
+        {
+            //Arrange
+            _httpContext.Request.Path = new PathString("/test");
+            _httpContext.Request.QueryString = new QueryString("?query");
+            _httpContext.Request.Method = "Get";
+            A.CallTo(() => _next.Invoke(A<HttpContext>.Ignored)).ThrowsAsync(new ValidationException("Validation error"));
+
+            //Act
+            await _errorHandlingMiddleware.Invoke(_httpContext);
+
+            //Assert
+            var message = $"Bad Request: Validation error - {_httpContext.Request.Method} {_httpContext.Request.Path + _httpContext.Request.QueryString}.";
+            AssertLogMethod(message, typeof(ValidationException), LogLevel.Warning);
+            Assert.Equal(400, _httpContext.Response.StatusCode);
+        }
+
+        [Fact]
+        public async Task Invoke_must_log_warning_and_set_HttpStatusCode_InternalServerError_on_any_other_exception()
+        {
+            //Arrange
+            _httpContext.Request.Path = new PathString("/test");
+            _httpContext.Request.QueryString = new QueryString("?query");
+            _httpContext.Request.Method = "Get";
+            A.CallTo(() => _next.Invoke(A<HttpContext>.Ignored)).ThrowsAsync(new ArgumentNullException("Argument missing"));
+
+            //Act
+            await _errorHandlingMiddleware.Invoke(_httpContext);
+
+            //Assert
+            var message = $"Error: Value cannot be null. (Parameter 'Argument missing').";
+            AssertLogMethod(message, typeof(ArgumentNullException), LogLevel.Error);
+            Assert.Equal(500, _httpContext.Response.StatusCode);
+        }
+
+        private void AssertLogMethod(string message, Type type, LogLevel logLevel)
+        {
+            A.CallTo(_logger).Where(x => x.Method.Name.Equals("Log")
+               && x.Arguments.Get<LogLevel>(0) == logLevel
+               && x.Arguments.Get<object>(2).ToString().Equals(message)
+               && x.Arguments.Get<object>(3).GetType() == type)
+               .MustHaveHappenedOnceExactly();
+        }
+    }
+}
